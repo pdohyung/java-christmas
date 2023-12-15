9953c4c095b5212e8fcb8779a222716ed127cc47
kimdohyung <pdohyung@gmail.com>
2023-12-15 21:53:46 +0900

    feat(OutputView): 혜택 내역을 출력한다.

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -7,8 +7,10 @@ public class OutputView {
     private static final String PRINT_START_EVENT_PREVIEW = "12월 {0}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String PRINT_START_ORDER_MENUS = "\n<주문 메뉴>";
     private static final String PRINT_START_GIFT = "\n<증정 메뉴>";
+    private static final String PRINT_START_DISCOUNT = "\n<혜택 내역>";
     private static final String PRINT_ORDER_MENU = "{0} {1}개";
     private static final String PRINT_TOTAL_PRICE = "\n<할인 전 총주문 금액>\n{0}원";
+    private static final String PRINT_DISCOUNT = "{0}: -{1}";
     private static final String PRINT_GIFT = "샴페인 1개";
     private static final String NONE = "없음";
 
@@ -38,6 +40,18 @@ public class OutputView {
             System.out.println(PRINT_GIFT);
             return;
         }
+        printNone();
+    }
+
+    public void printStartDiscount() {
+        System.out.println(PRINT_START_DISCOUNT);
+    }
+
+    public void printDiscount(String discountName, int discountPrice) {
+        System.out.println(format(PRINT_DISCOUNT, discountPrice, discountPrice));
+    }
+
+    public void printNone() {
         System.out.println(NONE);
     }
 }
