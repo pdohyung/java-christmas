04141b6e67bac63335a06e872eea5d7b2fff7a28
kimdohyung <pdohyung@gmail.com>
2023-12-15 21:35:38 +0900

    feat(OutputView): 증정 메뉴를 출력한다.

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -6,9 +6,11 @@ public class OutputView {
     private static final String PRINT_EVENT_START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String PRINT_START_EVENT_PREVIEW = "12월 {0}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String PRINT_START_ORDER_MENUS = "\n<주문 메뉴>";
-    private static final String PRINT_START_TOTAL_PRICE = "\n<할인 전 총주문 금액>";
+    private static final String PRINT_START_GIFT = "\n<증정 메뉴>";
     private static final String PRINT_ORDER_MENU = "{0} {1}개";
-    private static final String PRINT_TOTAL_PRICE = "{0}원";
+    private static final String PRINT_TOTAL_PRICE = "\n<할인 전 총주문 금액>\n{0}원";
+    private static final String PRINT_GIFT = "샴페인 1개";
+    private static final String NONE = "없음";
 
     public void printEventStart() {
         System.out.println(PRINT_EVENT_START_MESSAGE);
@@ -26,11 +28,16 @@ public class OutputView {
         System.out.println(format(PRINT_ORDER_MENU, menuName, quantity));
     }
 
-    public void printStartTotalPrice() {
-        System.out.println(PRINT_START_TOTAL_PRICE);
-    }
-
     public void printTotalPrice(int totalPrice) {
         System.out.println(format(PRINT_TOTAL_PRICE, totalPrice));
+    }
+
+    public void printGift(boolean hasGift) {
+        System.out.println(PRINT_START_GIFT);
+        if (hasGift) {
+            System.out.println(PRINT_GIFT);
+            return;
+        }
+        System.out.println(NONE);
     }
 }
